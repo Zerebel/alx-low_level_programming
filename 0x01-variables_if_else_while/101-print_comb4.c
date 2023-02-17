67a0649b67e0836246c135be4f5a9a714f600885
Zerebel Tanu <zerebeltanu@gmail.com>
2023-02-17 09:55:02 -0800

    fix variable error

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,8 +21,8 @@ int main(void)
 	{
 	putchar(num1 + '0');
 	putchar(num2 + '0');
-	putchar(num2 + '0');
-	if (num1 != 7 || num2 != 8 || num3 != 9)
+	putchar(num3 + '0');
+	if (num1 < 7 || num2 < 8 || num3 < 9)
 	{
 	putchar(',');
 	putchar(' ');
