6d72dab7e525b8ed66cfea42be230dab448635d9
Zerebel Tanu <zerebeltanu@gmail.com>
2023-02-21 12:02:49 -0800

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,32 @@
 */
 void print_to_98(int n)
 {
-{
 int i, j;
+
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+i = n;
+
+while (i <= 98)
 {
 if (i != 98)
 printf("%d, ", i);
 else if (i == 98)
 printf("%d\n", i);
+i++;
 }
 }
 else if (n >= 98)
 {
-for (j = n; j >= 98; j--)
+j = n;
+
+while (j >= 98)
 {
 if (j != 98)
 printf("%d, ", j);
 else if (j == 98)
 printf("%d\n", j);
+j--;
 }
 }
 }
