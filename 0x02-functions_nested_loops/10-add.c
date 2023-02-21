3557f0d3bc675805b1f3141ad60c8b34b143372a
Zerebel Tanu <zerebeltanu@gmail.com>
2023-02-21 11:52:31 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - ad  two numbers
+*
+* Return: null
+*/
+int add(int i, int j)
+{
+return (i + j);
+}
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
 *
 * Return: Null
 */
-void times_table(void);
+void times_table(void)
 {
 int i, j, k;
 for (i = 0; i < 10; i++)
