41185fd7190f441c7b553cacf31a1db92bb158b8
Zerebel Tanu <zerebeltanu@gmail.com>
2023-02-27 01:01:11 -0800

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* void reset_to_98(int *n); - checks
+*
+* @n: pointer to a var
+* Return: None
+*/
+void reset_to_98(int *n)
+{
+	* n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+
+/**
+* Main.h - header file
+*
+* All function files called here
+*/
+void reset_to_98(int *n);
