069eb281aeaad1de8441412098739a47e3cf7c00
Zerebel Tanu <zerebeltanu@gmail.com>
2023-02-27 01:03:01 -0800

    betty fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* void reset_to_98(int *n); - checks
+* reset_to_98 - reset value to 98
 *
 * @n: pointer to a var
 * Return: None
 */
 void reset_to_98(int *n)
 {
-	* n = 98;
+	*n = 98;
 }
