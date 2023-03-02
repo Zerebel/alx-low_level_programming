0e818ecd417b7557fa9e246e7b53f6c9c967a5e8
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-02 07:29:48 -0800

    betty fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,17 +2,18 @@
 /**
  * infinite_add - Adds two numbers.
  *
- * @n1: A string representation of the first number.
- * @n2: A string representation of the second number.
- * @r: The buffer to store the result in.
- * @size_r: The size of the result buffer.
+ * @num1: A string representation of the first number.
+ * @num2: A string representation of the second number.
+ * @result: The buffer to store the result in.
+ * @result_size: The size of the result buffer.
  *
  * Return: If the result fits in the buffer, return a pointer to @r.
  * else return 0.
  */
 char *infinite_add(char *num1, char *num2, char *result, int result_size)
 {
-int length_num1 = 0, length_num2 = 0, operation, carry = 0, big, digit_num1, digit_num2;
+int length_num1 = 0, length_num2 = 0;
+int operation, carry = 0, big, digit_num1, digit_num2;
 
 while (*(num1 + length_num1) != '\0')
 length_num1++;
@@ -27,8 +28,8 @@ return (0);
 
 result[big + 1] = '\0';
 length_num1--, length_num2--, result_size--;
-digit_num1 = *(num1 + length_num1) - '0';
-digit_num2 = *(num2 + length_num2) - '0';
+digit_num1 = *(num1 + length_num1) -'0';
+digit_num2 = *(num2 + length_num2) -'0';
 while (big >= 0)
 {
 operation = digit_num1 + digit_num2 + carry;
@@ -41,11 +42,11 @@ if (operation > 0)
 else
 *(result + big) = '0';
 if (length_num1 > 0)
-length_num1--, digit_num1 = *(num1 + length_num1) - '0';
+length_num1--, digit_num1 = *(num1 + length_num1) -'0';
 else
 digit_num1 = 0;
 if (length_num2 > 0)
-length_num2--, digit_num2 = *(num2 + length_num2) - '0';
+length_num2--, digit_num2 = *(num2 + length_num2) -'0';
 else
 digit_num2 = 0;
 big--, result_size--;
