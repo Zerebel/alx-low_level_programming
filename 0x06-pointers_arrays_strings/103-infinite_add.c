7a29edc6080a3ad069c31771e1d193e9a441d914
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-02 08:05:55 -0800

    betty fixed

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,27 @@
-#include "main.h"
 /**
- * infinite_add - Adds two numbers.
+ * infinite_add - Adds two numbers stored as strings.
  *
- * @num1: A string representation of the first number.
- * @num2: A string representation of the second number.
- * @result: The buffer to store the result in.
- * @result_size: The size of the result buffer.
+ * @num1: First number.
+ * @num2: Second number.
+ * @result: Buffer to store the result.
+ * @result_size: Size of the buffer.
  *
- * Return: If the result fits in the buffer, return a pointer to @r.
- * else return 0.
+ * Return: If buffer size is too small return 0, else return the sum
  */
 char *infinite_add(char *num1, char *num2, char *result, int result_size)
 {
-int length_num1 = 0, length_num2 = 0;
-int operation, carry = 0, big, digit_num1, digit_num2;
-
-while (*(num1 + length_num1) != '\0')
+int length_num1 = 0, length_num2 = 0, operation, carry = 0;
+int big, digit_num1, digit_num2;
+/* Get length of num1 and num2 */
+while (*(num1 + length_num1))
 length_num1++;
-while (*(num2 + length_num2) != '\0')
+while (*(num2 + length_num2))
 length_num2++;
-if (length_num1 >= length_num2)
-big = length_num1;
-else
-big = length_num2;
+/* Determine which number is bigger */
+big = (length_num1 >= length_num2) ? length_num1 : length_num2;
+/* If buffer size is too small return 0 */
 if (result_size <= big + 1)
 return (0);
-
 result[big + 1] = '\0';
 length_num1--, length_num2--, result_size--;
 digit_num1 = *(num1 + length_num1) -'0';
@@ -33,27 +29,15 @@ digit_num2 = *(num2 + length_num2) -'0';
 while (big >= 0)
 {
 operation = digit_num1 + digit_num2 + carry;
-if (operation >= 10)
-carry = operation / 10;
-else
-carry = 0;
-if (operation > 0)
-*(result + big) = (operation % 10) + '0';
-else
-*(result + big) = '0';
-if (length_num1 > 0)
-length_num1--, digit_num1 = *(num1 + length_num1) -'0';
-else
-digit_num1 = 0;
-if (length_num2 > 0)
-length_num2--, digit_num2 = *(num2 + length_num2) -'0';
-else
-digit_num2 = 0;
+carry = (operation >= 10) ? operation / 10 : 0;
+*(result + big) = (operation > 0) ? ((operation % 10) + '0') : '0';
+digit_num1 = (length_num1 > 0) ? *(--num1 + length_num1 - 1) - '0' : 0;
+digit_num2 = (length_num2 > 0) ? *(--num2 + length_num2 - 1) - '0' : 0;
 big--, result_size--;
 }
-if (*result == '0')
-return (result + 1);
-else
+/* Remove leading zeros */
+while (*result == '0' && *(result + 1) != '\0')
+result++;
 return (result);
 }
 
