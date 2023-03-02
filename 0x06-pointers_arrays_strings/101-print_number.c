d316982dcb6801de12baa362b24108f374f41420
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-02 02:45:49 -0800

    fix betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,7 +7,8 @@ void print_number(int n)
 {
 unsigned int num;
 
-if (n < 0) {
+if (n < 0)
+{
 _putchar('-');
 num = -n;
 }
@@ -15,7 +16,8 @@ else
 {
 num = n;
 }
-if (num / 10) {
+if (num / 10)
+{
 print_number(num / 10);
 }
 _putchar((num % 10) + '0');
