d463888e8cca856929d5bd3e503a71d16cf47fc8
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-14 01:55:26 -0700

    betty fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,47 +12,37 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	char *tempArr1 = s1, *tempArr2 = s2, *start, *array;
+	char *start, *array;
 	unsigned int len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 	{
 	s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 	s2 = "";
 	}
-
-	while (*tempArr1 != '\0')
+	while (s1[len1] != '\0')
 	{
 	len1++;
-	tempArr1++;
 	}
-
-	while (*tempArr2 != '\0')
+	while (s2[len2] != '\0')
 	{
 	len2++;
-	tempArr2++;
 	}
-
 	array = malloc(sizeof(char) * (len1 + len2 + 1));
-
 	if (array == NULL)
 	{
 	return (NULL);
 	}
-
 	start = array;
-
 	while (*s1 != '\0')
 	{
 	*array = *s1;
 	array++;
 	s1++;
 	}
-
 	while (*s2 != '\0')
 	{
 	*array = *s2;
@@ -60,7 +50,5 @@ char *str_concat(char *s1, char *s2)
 	s2++;
 	}
 	*array = '\0';
-
 return (start);
 }
-
