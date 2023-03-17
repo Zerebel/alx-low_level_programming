aa295bce63a5cb4733071f38d1d6f82d7a672b14
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-16 17:40:18 -0700

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,19 +1,18 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <limits.h>
+#include <string.h>
 
 /**
-* main - Multiplies two positive numbers
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 on success, 98 on error
+* main - Multiplies two positive numbers.
+* @argc: The number of arguments.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 if successful, 98 otherwise.
 */
 int main(int argc, char **argv)
 {
-int num1, num2, result;
-char *endptr;
+char *num1, *num2, *result;
+int len1, len2, i, j, carry, digit;
 
 if (argc != 3)
 {
@@ -21,24 +20,47 @@ printf("Error\n");
 return (98);
 }
 
-errno = 0;
-num1 = strtol(argv[1], &endptr, 10);
-if (errno != 0 || *endptr != '\0' || num1 > INT_MAX || num1 < 0)
+num1 = argv[1];
+num2 = argv[2];
+len1 = strlen(num1);
+len2 = strlen(num2);
+result = calloc(len1 + len2 + 1, sizeof(char));
+
+if (result == NULL)
+return (98);
+
+for (i = len1 - 1; i >= 0; i--)
+{
+if (num1[i] < '0' || num1[i] > '9')
 {
 printf("Error\n");
+free(result);
 return (98);
 }
 
-errno = 0;
-num2 = strtol(argv[2], &endptr, 10);
-if (errno != 0 || *endptr != '\0' || num2 > INT_MAX || num2 < 0)
+carry = 0;
+
+for (j = len2 - 1; j >= 0; j--)
+{
+if (num2[j] < '0' || num2[j] > '9')
 {
 printf("Error\n");
+free(result);
 return (98);
 }
 
-result = num1 *num2;
-printf("%d\n", result);
+digit = (num1[i] - '0') * (num2[j] - '0') + carry + result[i + j + 1] - '0';
+result[i + j + 1] = digit % 10 + '0';
+carry = digit / 10;
+}
+
+result[i + j + 1] = carry + '0';
+}
+
+while (*result == '0' && *(result + 1) != '\0')
+result++;
+
+printf("%s\n", result);
 
 return (0);
 }
