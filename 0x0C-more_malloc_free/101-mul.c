9a5ef0ad7a4c37024922d24136b5e11c61c395de
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-16 17:03:43 -0700

    new

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,28 +1,45 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 
 /**
-* main - Check the code
+* main - Multiplies two positive numbers
 * @argc: number of arguments
 * @argv: arguments
-* Return: Nothing
+* Return: 0 on success, 98 on error
 */
 int main(int argc, char **argv)
 {
 int num1, num2, result;
+char *endptr;
 
 if (argc != 3)
 {
 printf("Error\n");
-exit(98);
+return (98);
 }
-else
+
+errno = 0;
+num1 = strtol(argv[1], &endptr, 10);
+if (errno != 0 || *endptr != '\0' || num1 > INT_MAX || num1 < 0)
 {
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
+printf("Error\n");
+return (98);
+}
+
+errno = 0;
+num2 = strtol(argv[2], &endptr, 10);
+if (errno != 0 || *endptr != '\0' || num2 > INT_MAX || num2 < 0)
+{
+printf("Error\n");
+return (98);
+}
+
 result = num1 *num2;
 printf("%d\n", result);
+
 return (0);
 }
-}
+
