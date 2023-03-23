c4728e791125ac32cf37b4b16635646b1ce68d4d
Zerebel Tanu <zerebeltanu@gmail.com>
2023-03-23 12:28:53 -0700

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,7 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list args;
-int sum = 0;
-unsigned int i;
+unsigned int i, sum = 0;
 
 if (n == 0)
 return (0);
@@ -20,7 +19,7 @@ return (0);
 va_start(args, n);
 
 for (i = 0; i < n; i++)
-sum += va_arg(args, int);
+sum += va_arg(args, const unsigned int);
 
 va_end(args);
 
